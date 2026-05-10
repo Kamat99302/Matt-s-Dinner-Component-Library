523efcf14c4720f56e0637144e4d1cca5f688a17
Kamat99302 <matthieu.juan@gmail.com>
2026-05-10 23:04:16 +0200

    refacto: CategoryTab CSS to BEM

--- src/components/CategoryTab/CategoryTab.jsx
+++ src/components/CategoryTab/CategoryTab.jsx
@@ -1,16 +1,16 @@
 import './CategoryTab.css'
 export default function CategoryTab({categories = ['Burgers', 'Sides', 'Beverages', 'Desserts'], activeIndex, onCategoryClick, onIndexClick}){
     return(
-        <nav>
-            <ul>
+        <nav className='category-tab'>
+            <ul className='category-tab__list'>
                 {categories.map((category, index)=>(
-                    <li 
+                    <li
                         onClick={()=>{
                             onCategoryClick(category.toLowerCase())
                             onIndexClick(index)
                         }} 
                         key={index}
-                        className={ index === activeIndex? 'highlighted': ''}>
+                        className={ index === activeIndex? 'category-tab__item category-tab__item--active': 'category-tab__item'}>
                             {category}
                     </li>
                 ))}
